6c11a635483da4d6ed47b0fc4131b83e4b2a13da
James Moore <hello@jmoore.me>
2012-01-10 19:10:14 -0800

    Initial Commit

---
+++ Last Time/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Last Time
+//
+//  Created by James Moore on 1/10/12.
+//  Copyright (c) 2012 Self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, nil);
+	}
+}
---
+++ Last TimeTests/Last_TimeTests.h
@@ -0,0 +1,13 @@
+//
+//  Last_TimeTests.h
+//  Last TimeTests
+//
+//  Created by James Moore on 1/10/12.
+//  Copyright (c) 2012 Self. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Last_TimeTests : SenTestCase
+
+@end
