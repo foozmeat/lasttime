0492526695de85a983977ddc9b26ff2571e192ea
James Moore <hello@jmoore.me>
2012-01-10 19:24:58 -0800

    renamed main classes

---
+++ Last Time/LastTimeAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LastTimeAppDelegate.h
+//  LastTime
+//
+//  Created by James Moore on 1/10/12.
+//  Copyright (c) 2012 Self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LastTimeAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+@end
