e5380182e89e93b0b12da77a063a34c61951d3c7
James Moore <hello@jmoore.me>
2012-01-12 21:39:19 -0800

    start hooking up table view

---
+++ Last Time/Controllers/LogEntryViewController.m
@@ -0,0 +1,23 @@
+//
+//  LogEntryViewController.m
+//  Last Time
+//
+//  Created by James Moore on 1/12/12.
+//  Copyright (c) 2012 Self. All rights reserved.
+//
+
+#import "LogEntryViewController.h"
+
+@implementation LogEntryViewController
+- (id) init
+{
+	self = [super initWithStyle:UITableViewStyleGrouped];
+	
+	return self;
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+	return [self init];
+}
+@end
