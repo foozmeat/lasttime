49eb15e431ba5489ef78c00f4228d85eb8071c19
James Moore <hello@jmoore.me>
2012-03-02 08:15:10 -0800

    don't create a log entry by default

--- Last Time/Models/Event.m
+++ Last Time/Models/Event.m
@@ -14,7 +14,7 @@
 - (id)init
 {
 	return [self initWithEventName:@"" 
-											logEntries:[[NSMutableArray alloc] initWithObjects:[[LogEntry alloc] init], nil]];
+											logEntries:[[NSMutableArray alloc] init]];
 }
 
 - (id)initWithEventName:(NSString *)name logEntries:(NSMutableArray *)entries
