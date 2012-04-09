6c1cdd27717e88969102593f68db2e4d5967f18b
James Moore <hello@jmoore.me>
2012-04-08 17:51:55 -0700

    update to TestFlight

--- Last Time/Controllers/LastTimeAppDelegate.m
+++ Last Time/Controllers/LastTimeAppDelegate.m
@@ -25,6 +25,9 @@
 
 #ifdef TESTFLIGHT
 	[TestFlight takeOff:@"7dc090a5932acba7bcf3c281394f4a6a_NTAwNTgyMDExLTEyLTI3IDE2OjM1OjAyLjE4Mzg5MA"];
+#ifdef SEND_UDID
+	[TestFlight setDeviceIdentifier:[[UIDevice currentDevice] uniqueIdentifier]];
+#endif
 #endif
 	
 	[self versionCheck];
