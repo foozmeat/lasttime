afbe68b009c9803d7952c2d1c890785cd521fe71
James Moore <hello@jmoore.me>
2013-06-12 20:51:31 -0700

    no brown

--- Last Time/Support/DatePickerCell.m
+++ Last Time/Support/DatePickerCell.m
@@ -26,8 +26,8 @@
 	[pickerView addTarget:self action:@selector(dateChanged:) forControlEvents:UIControlEventValueChanged];
 	
 	[[self detailTextLabel] setText:[df stringFromDate:[pickerView date]]];
-	self.detailTextLabel.textColor = [UIColor brownColor];
-	
+//	self.detailTextLabel.textColor = [UIColor brownColor];
+
 	if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
 		UIViewController *datePickerViewController = [[UIViewController alloc] init];
 		
