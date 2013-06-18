0f83223db7725540762229a8d12d6c1dfdc5f348
James Moore <james@panic.com>
2013-06-17 17:14:16 -0700

    removing cruft

--- Last Time/Controllers/LastTimeAppDelegate.h
+++ Last Time/Controllers/LastTimeAppDelegate.h
@@ -8,16 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-//@class MGSplitViewController;
-//@class SegmentsController;
-
 @interface LastTimeAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) IBOutlet UIWindow *window;
-
-//@property (strong, nonatomic) MGSplitViewController *splitViewController;
-//@property (nonatomic, strong) SegmentsController * segmentsController;
-//@property (nonatomic, strong) UISegmentedControl * segmentedControl;
 
 @end
 
--- Last Time/Controllers/LastTimeAppDelegate.m
+++ Last Time/Controllers/LastTimeAppDelegate.m
@@ -17,8 +17,6 @@
 @implementation LastTimeAppDelegate
 
 @synthesize window;
-//@synthesize splitViewController;
-//@synthesize segmentsController, segmentedControl;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -37,80 +35,10 @@
 	UILocalNotification *localNotif = [launchOptions objectForKey:UIApplicationLaunchOptionsLocalNotificationKey];
 	[self handleNotification:localNotif];
 	
-//	NSArray *viewControllers = [self segmentViewControllers];
-//	NSArray *segmentTitles = [[NSArray alloc] initWithObjects:NSLocalizedString(@"Lists",@"Lists"), NSLocalizedString(@"Timeline",@"Timeline"), nil];
-//
-//	UIView *backgroundView = [[UIView alloc] initWithFrame: window.frame];
-//	backgroundView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"paper.jpg"]];
-//	[window addSubview:backgroundView];
-
-//	if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-//
-//        UINavigationController *navigationController = [[UINavigationController alloc] init];
-//        self.segmentsController = [[SegmentsController alloc] initWithNavigationController:navigationController viewControllers:viewControllers];
-//        
-//        self.segmentedControl = [[UISegmentedControl alloc] initWithItems:segmentTitles];
-//        self.segmentedControl.segmentedControlStyle = UISegmentedControlStyleBar;
-//
-//        [self.segmentedControl addTarget:self.segmentsController
-//                                  action:@selector(indexDidChangeForSegmentedControl:)
-//                        forControlEvents:UIControlEventValueChanged];
-//        
-//        [self firstUserExperience];
-//
-//        window.rootViewController = navigationController;
-//    [window addSubview:navigationController.view];
-
-//	} else {
-//		FolderListViewController *masterViewController = [viewControllers objectAtIndex:0];
-//		
-//		UINavigationController *masterNavigationController = [[UINavigationController alloc] initWithRootViewController:masterViewController];
-//
-//		EventController *detailViewController = masterViewController.detailViewController;
-//		UINavigationController *detailNavigationController = [[UINavigationController alloc] initWithRootViewController:detailViewController];
-//		
-//		self.segmentsController = [[SegmentsController alloc] initWithNavigationController:masterNavigationController viewControllers:viewControllers];
-//    
-//        self.segmentedControl = [[UISegmentedControl alloc] initWithItems:segmentTitles];
-//        self.segmentedControl.segmentedControlStyle = UISegmentedControlStyleBar;
-//        
-//        [self.segmentedControl addTarget:self.segmentsController
-//                                  action:@selector(indexDidChangeForSegmentedControl:)
-//                        forControlEvents:UIControlEventValueChanged];
-//        
-//        [self firstUserExperience];
-//                    
-//		self.splitViewController = [[MGSplitViewController alloc] init];
-//		self.splitViewController.showsMasterInPortrait = YES;
-//
-//		self.splitViewController.viewControllers = [NSArray arrayWithObjects:masterNavigationController, detailNavigationController, nil];
-//		
-//		self.window.rootViewController = self.splitViewController;
-//
-//	}
 	[self customizeAppearance];
 
-//	[window makeKeyAndVisible];
 	return YES;
 }
-
-//- (NSArray *)segmentViewControllers {
-//	FolderListViewController *lists = [[FolderListViewController alloc] init];
-//	TimelineViewController *timeline = [[TimelineViewController alloc] init];
-//	EventController *detailViewController = [[EventController alloc] init];
-//
-//	lists.detailViewController = detailViewController;
-//	timeline.detailViewController = detailViewController;
-//	
-//	NSArray *viewControllers = [NSArray arrayWithObjects:lists, timeline, nil];
-//
-//	return viewControllers;
-//}
-
-//- (void)firstUserExperience {
-//	self.segmentedControl.selectedSegmentIndex = 0;
-//	[self.segmentsController indexDidChangeForSegmentedControl:self.segmentedControl];
-//}
 
 - (void)application:(UIApplication *)app didReceiveLocalNotification:(UILocalNotification *)localNotification {
 	[self handleNotification:localNotification];
@@ -159,8 +87,6 @@
 
 - (void)customizeAppearance
 {
-	
-//	[[UIBarButtonItem appearance] setTintColor:[UIColor blueColor]];
 
     [[UINavigationBar appearance] setTitleTextAttributes:
      [NSDictionary dictionaryWithObjectsAndKeys:
@@ -191,60 +117,10 @@
 	[[UIBarButtonItem appearance] setBackButtonBackgroundImage:backButton forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
 
     [[UINavigationBar appearance] setTintColor:[UIColor colorWithRed:217.0/255.0 green:217.0/255.0 blue:217.0/255.0 alpha:1.0]];
-//    [[UINavigationBar appearance] setTintColor:[UIColor redColor]];
-
-//	[[UITabBar appearance] setTintColor:[UIColor brownColor]];
-
-//	UIImage *barButton = [[UIImage imageNamed:@"leather_navbar_button.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 4, 0, 4)];
-//
-
-
-//	[[UISegmentedControl appearance] setTintColor:[UIColor brownColor]];
-
-//	UIImage *segmentSelected = [[UIImage imageNamed:@"leather_seg_sel.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 4, 0, 4)];
-//	UIImage *segmentUnselected = [[UIImage imageNamed:@"leather_seg_uns.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 4, 0, 4)];
-//	UIImage *segmentSelectedUnselected = [UIImage imageNamed:@"leather_seg_sel-uns.png"];
-//	UIImage *segUnselectedSelected = [UIImage imageNamed:@"leather_seg_uns-sel.png"];
-//	UIImage *segmentUnselectedUnselected = [UIImage imageNamed:@"leather_seg_uns-uns.png"];
-
-//	[[UISegmentedControl appearance] setBackgroundImage:segmentUnselected forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
-//	[[UISegmentedControl appearance] setBackgroundImage:segmentSelected forState:UIControlStateSelected barMetrics:UIBarMetricsDefault];
-
-//	[[UISegmentedControl appearance] setDividerImage:segmentUnselectedUnselected
-//															 forLeftSegmentState:UIControlStateNormal
-//																 rightSegmentState:UIControlStateNormal
-//																				barMetrics:UIBarMetricsDefault];
-//	[[UISegmentedControl appearance] setDividerImage:segmentSelectedUnselected
-//															 forLeftSegmentState:UIControlStateSelected
-//																 rightSegmentState:UIControlStateNormal
-//																				barMetrics:UIBarMetricsDefault];
-//
-//	[[UISegmentedControl appearance] setDividerImage:segUnselectedSelected
-//															 forLeftSegmentState:UIControlStateNormal
-//																 rightSegmentState:UIControlStateSelected
-//																				barMetrics:UIBarMetricsDefault];
-
-
-
-    // iPhone
-//    UIImage *navBarImage = [UIImage imageNamed:@"leather_navbar.png"];
-//    [[UINavigationBar appearance] setBackgroundImage:navBarImage forBarMetrics:UIBarMetricsDefault];
-//
-//    UIImage* tabBarBackground = [UIImage imageNamed:@"leather_toolbar.png"];
-//    [[UIToolbar appearance] setBackgroundImage:tabBarBackground forToolbarPosition:UIToolbarPositionBottom barMetrics:UIBarMetricsDefault];
-
-
 }
 
-- (BOOL)application:(UIApplication *)application
-						openURL:(NSURL *)url
-	sourceApplication:(NSString *)sourceApplication
-				 annotation:(id)annotation {
-//	NSLog(@"url recieved: %@", url);
-//	NSLog(@"scheme: %@", [url scheme]);
-//	NSLog(@"query string: %@", [url query]);
-
-		// handle these things in your app here
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
+{
 	return YES;
 }
 
