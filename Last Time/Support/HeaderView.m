a621b886cdf666912105fe776e59cbc7d7250cd3
James Moore <james@panic.com>
2013-06-20 17:07:14 -0700

    table header

--- Last Time/Support/HeaderView.m
+++ Last Time/Support/HeaderView.m
@@ -9,7 +9,7 @@
 #import "HeaderView.h"
 #import <QuartzCore/QuartzCore.h>
 
-static CGFloat height = 21;
+static CGFloat height = 28;
 
 @implementation HeaderView
 @synthesize headerLabel;
@@ -19,6 +19,7 @@ static CGFloat height = 21;
 	CGRect frame = CGRectMake(0, 0, width, height);
 	self = [self initWithFrame:frame];
 	self.headerLabel = label;
+    self.backgroundColor = [UIColor clearColor];
 	
 	return self;
 }
@@ -32,65 +33,20 @@ static CGFloat height = 21;
     return self;
 }
 
-//- (CAGradientLayer *) brownGradient {
-//	CAGradientLayer *gradient = [CAGradientLayer layer];
-//	gradient.startPoint = CGPointMake(0.5, 0.0);
-//	gradient.endPoint = CGPointMake(0.5, 1.0);
-//
-////	UIColor *color1 = [UIColor brownColor];
-//	UIColor *color2 = [UIColor colorWithRed:203.0f/255.0f green:177.0f/255.0f blue:151.0f/255.0f alpha:1.0];
-//	UIColor *color2 = [UIColor whiteColor];
-//
-//	[gradient setColors:[NSArray arrayWithObjects:(id)color1.CGColor, (id)color2.CGColor, nil]];
-//	return gradient;
-//}
-
 - (void)drawRect:(CGRect)rect
 {
+    LTStyleManager *sm = [LTStyleManager new];
+    UIView* headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, rect.size.width, height)];
 
-    LTStyleManager *sm = [LTStyleManager manager];
-
-	[self setBackgroundColor:[UIColor clearColor]];
-
-	UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0,0, rect.size.width,height)];
+	UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10,0, rect.size.width,height)];
 	label.text = self.headerLabel;
-	label.textAlignment = UITextAlignmentCenter;
-	label.backgroundColor = [sm tableHeaderColor];
-    label.font = [sm mediumFontWithSize:17.0];
+	label.textAlignment = UITextAlignmentLeft;
+    label.backgroundColor = [sm tableHeaderColor];
+    label.font = [sm cellLabelFontWithSize:17.0];
 
-	[self addSubview:label];
+    [headerView addSubview:label];
 
-//	CAGradientLayer *gradient = [self brownGradient];
-//	gradient.frame = self.bounds;
-//	[self.layer addSublayer:gradient];
-//
-//	CALayer *topLine = [CALayer layer];
-//	topLine.frame = CGRectMake(0, 0, rect.size.width, 1);
-//	topLine.backgroundColor = [UIColor grayColor].CGColor;
-//	[self.layer addSublayer:topLine];
-//	
-//	CALayer *border = [CALayer layer];
-//	border.borderColor = [UIColor brownColor].CGColor;
-//	border.borderColor = [[UIColor colorWithRed:203.0f/255.0f green:177.0f/255.0f blue:151.0f/255.0f alpha:1.0] CGColor];
-//
-//	border.borderWidth = 1;
-//	border.frame = CGRectMake(-1, -1, self.frame.size.width + 2, self.frame.size.height+2);
-//	
-//	[self.layer addSublayer:border];
-//
-//    LTStyleManager *sm = [LTStyleManager manager];
-//
-//	UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0,0, rect.size.width,height)];
-//	label.text = self.headerLabel;
-//	label.textAlignment = UITextAlignmentCenter;
-//	label.backgroundColor = [UIColor clearColor];
-//    label.font = [sm mediumFontWithSize:17.0];
-//
-//	label.textColor = [UIColor whiteColor];
-//	label.shadowColor = [UIColor darkGrayColor];
-//	label.shadowOffset = CGSizeMake(0, 1);
-//	
-//	[self addSubview:label];
+	[self addSubview:headerView];
 
 }
 
