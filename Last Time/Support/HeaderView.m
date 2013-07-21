1e7c3a05aeed824b09060d9fc66d5016d2127f02
James Moore <hello@jmoore.me>
2013-07-21 15:39:11 -0700

    formatting

--- Last Time/Support/HeaderView.m
+++ Last Time/Support/HeaderView.m
@@ -19,33 +19,33 @@ static CGFloat height = 28;
 	CGRect frame = CGRectMake(0, 0, width, height);
 	self = [self initWithFrame:frame];
 	self.headerLabel = label;
-    LTStyleManager *sm = [LTStyleManager new];
-    self.backgroundColor = [sm tableHeaderColor];
-	
+  LTStyleManager *sm = [LTStyleManager new];
+  self.backgroundColor = [sm tableHeaderColor];
+
 	return self;
 }
 
 - (id)initWithFrame:(CGRect)frame
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
-    }
-    return self;
+  self = [super initWithFrame:frame];
+  if (self) {
+    // Initialization code
+  }
+  return self;
 }
 
 - (void)drawRect:(CGRect)rect
 {
-    LTStyleManager *sm = [LTStyleManager new];
-    UIView* headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, rect.size.width, height)];
+  LTStyleManager *sm = [LTStyleManager new];
+  UIView* headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, rect.size.width, height)];
 
 	UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10,0, rect.size.width,height)];
 	label.text = self.headerLabel;
 	label.textAlignment = UITextAlignmentLeft;
-    label.backgroundColor = [sm tableHeaderColor];
-    label.font = [sm cellLabelFontWithSize:17.0];
+  label.backgroundColor = [sm tableHeaderColor];
+  label.font = [sm cellLabelFontWithSize:17.0];
 
-    [headerView addSubview:label];
+  [headerView addSubview:label];
 
 	[self addSubview:headerView];
 
