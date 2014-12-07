c4744766d9a9891f8c53be9721139f2174517573
James Moore <james@panic.com>
2014-12-07 12:47:59 -0800

    snapshot

---
+++ snapshot.js
@@ -0,0 +1,30 @@
+#import "SnapshotHelper.js"
+
+var target = UIATarget.localTarget();
+
+captureLocalizedScreenshot("01-Lists")
+
+target.frontMostApp().navigationBar().buttons()[1].tap();
+target.frontMostApp().mainWindow().tableViews()[1].tapWithOptions({tapOffset:{x:0.40, y:0.20}});
+captureLocalizedScreenshot("02-Timeline")
+
+target.frontMostApp().navigationBar().buttons()[1].tap();
+target.frontMostApp().mainWindow().tableViews()[1].tapWithOptions({tapOffset:{x:0.40, y:0.10}});
+target.frontMostApp().mainWindow().tableViews()[0].cells()["Health"].tap();
+target.delay(1)
+captureLocalizedScreenshot("03-Event_List")
+
+target.frontMostApp().mainWindow().tableViews()[0].cells()["Running"].tap();
+target.delay(1)
+captureLocalizedScreenshot("04-Event_Detail")
+
+target.frontMostApp().navigationBar().leftButton().tap();
+target.frontMostApp().navigationBar().leftButton().tap();
+target.frontMostApp().mainWindow().tableViews()[0].cells()["Car"].tap();
+target.delay(1)
+target.frontMostApp().navigationBar().rightButton().tap();
+target.delay(1)
+target.frontMostApp().mainWindow().tableViews()[0].cells()["Checked Oil"].tap();
+target.delay(2)
+captureLocalizedScreenshot("05-Edit_Event")
+target.delay(1)
